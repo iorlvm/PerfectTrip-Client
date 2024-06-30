fe33fbc0a2b2d780bda16f9470ebef0d2a1c04e0
Alice <alitsen5151@gmail.com>
2024-06-30 13:38:43 +0800

    important message

--- src/views/StoreManageCenter/components/ImportantMessage.vue
+++ src/views/StoreManageCenter/components/ImportantMessage.vue
@@ -1,11 +1,48 @@
 <template>
     <div>
-        我是重要訊息
+        <div class="coupon">
+            <div class="brcoupon">2024-06-28
+                <h3>折價券到期提醒</h3>
+                <p>您帳號有兄弟大飯店折價券即將於2024-07-01到期，可至票券匣頁面查看並請盡快使用。</p>
+            </div>
+            <br>
+            <br>
+            <div class="clcoupon">2024-06-28
+                <h3>折價券到期提醒</h3>
+                <p>您帳號有長榮桂冠酒店折價券即將於2024-07-15到期，可至票券匣頁面查看並請盡快使用。</p>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.coupon {
+
+
+
+
+    .brcoupon {
+        display: flex;
+        justify-content: space-between;
+        padding: 40px;
+        border: 1px solid #0000002a;
+        border-radius: 8px;
+
+    }
+
+    .clcoupon {
+        display: flex;
+        justify-content: space-between;
+        padding: 40px;
+        border: 1px solid #0000002a;
+        border-radius: 8px;
+
+    }
+
+}
+</style>
