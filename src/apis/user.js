53507d167abddedf9670d42553353021d6d156f3
Felicie <felicieavbg@gmail.com>
2024-09-07 13:01:43 +0800

    增加會員註冊頁面

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,5 +1,30 @@
 import httpInstance from "@/utils/axiosInit";
 
+//會員註冊
+export const userRegisterAPI = ({firstName, lastName, nickName, gender,username,password, address, phoneNumber, taxId, birthday }) => {
+    return httpInstance({
+        url:'/users/register',
+        method: 'POST',
+        data: {
+            firstName, 
+            lastName, 
+            nickName, 
+            gender, 
+            address, 
+            username,
+            password,
+            phoneNumber,
+            taxId,
+            birthday
+            
+        }
+    })
+}
+
+
+
+
+
 //會員登入
 export const userLoginAPI = ({ username, password }) => {
     return httpInstance({
